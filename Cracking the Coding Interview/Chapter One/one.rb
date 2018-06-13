5fa74e37ed46e48bb5b13b01f83b0a18a8f2e2ee
Laurie Linz <laura.linz@ca.com>
2018-06-13 13:15:54 -0600

    The first part of is unique. TODO no additional data structures

---
+++ Cracking the Coding Interview/Chapter One/one.rb
@@ -0,0 +1,34 @@
+# Problem "Is unique"
+# Implement an algorithm to determine if a string has unique characters
+
+unique_chars_in_string = "abcdefghijklmnop1293!&"
+non_unique_chars_in_string = "aabc29930h^&"
+
+
+# Assume ASCII character set?
+def is_isogram(candidate_string)
+    standard_ascii_max_char_count = 128
+    char_counts = Array.new(128, 0)
+    isogram = true
+
+    if candidate_string.length > standard_ascii_max_char_count
+        return false
+    end
+
+    candidate_string.split("").each do |char|
+        if char_counts[char.ord] > 0
+            isogram = false
+            break
+        end
+        char_counts[char.ord] += 1
+    end
+
+    return isogram
+end
+
+puts "tests...."
+puts "#{unique_chars_in_string} #{is_isogram(unique_chars_in_string) ? 'is' : 'is not'} an isogram"
+puts "#{non_unique_chars_in_string} #{is_isogram(non_unique_chars_in_string) ? 'is' : 'is not'} an isogram"
+
+
+# What if you cannot use additional datastructures
